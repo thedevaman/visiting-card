79d79a850393eed6299d61c472b2178ad6ec3540
thedevaman <devaman2508@gmail.com>
2025-11-28 09:09:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Image } from "lucide-react"
+import React from "react"
+
+
+const App = () =>{
+
+  return(
+  <div className="bg-gray-200 h-screen">
+    <div className="bg-white rounded-xl p-4">
+     <button><Image/></button>
+    </div>
+  </div>
+  )
+
+}
+
+export default App
